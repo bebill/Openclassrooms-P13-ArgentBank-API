2c277a9b8ff1ca6ba278da05414e96abecc6a9dd
Ben Bld <bld.prom@gmail.com>
2024-03-20 21:02:52 +0100

    chore: use mongo atlas (online version)

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,7 +4,7 @@ const databaseUrl =
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
